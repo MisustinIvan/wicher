9c130954b76f2eead86507ae81635c06ab8dc158
MisustinIvan <ivanmis596@gmail.com>
2025-05-02 15:20:19 +0200

    switch to feh

--- main.c
+++ main.c
@@ -14,10 +14,10 @@ static char* filepaths[MAX_FILES];
 int filepath_count = 0;
 int filepath_index = 0;
 
-void set_wallpaper(const char path[], int dpy) {
+void set_wallpaper(const char path[]) {
     static char buffer[256];
     memset(buffer, 0, sizeof(buffer));
-    sprintf(buffer, "nitrogen --head=%d --set-zoom-fill \"%s\"", dpy, path);
+    sprintf(buffer, "feh --bg-fill \"%s\"", path);
 
     pid_t pid = fork();
     if (pid < 0) {
@@ -97,28 +97,7 @@ void cleanup() {
 
 int schedule(int delay_seconds) {
     while (true) {
-        static char buffer[128];
-#define cmd "xrandr --listactivemonitors | wc -l"
-
-        FILE* fp;
-        int monitor_count;
-
-        fp = popen(cmd, "r");
-        if (fp == NULL) {
-            perror("popen failed\n");
-            return EXIT_FAILURE;
-        }
-
-        if (fgets(buffer, sizeof(buffer), fp) != NULL) {
-            monitor_count = atoi(buffer);
-            monitor_count -= 1;
-        }
-
-        pclose(fp);
-
-        for (int monitor=0; monitor < monitor_count; monitor++) {
-            set_wallpaper(filepaths[filepath_index], monitor);
-        }
+        set_wallpaper(filepaths[filepath_index]);
         filepath_index = (filepath_index + 1) % filepath_count;
         sleep(delay_seconds);
     }
